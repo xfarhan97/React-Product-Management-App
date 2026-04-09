6f8452170f2c7a3a9c5dd8f60c9a17c0e2f177b0
xfarhan97 <xfarhan97@gmail.com>
2026-04-09 19:21:13 +0500

    test render

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProductList from "./pages/ProductList";
-import ProductDetails from "./pages/ProductDetails";
-import AddProduct from "./pages/AddProduct";
-import EditProduct from "./pages/EditProduct";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/add" element={<AddProduct />} />
-        <Route path="/edit/:id" element={<EditProduct />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>App Working on Vercel ✅</h1>;
 }
 
 export default App;
