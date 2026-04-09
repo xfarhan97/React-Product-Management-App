ef93e8ea6aaa3764e98b90c2de16d2fc707203f5
xfarhan97 <xfarhan97@gmail.com>
2026-04-09 19:25:59 +0500

    render

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
+import AddProduct from "./pages/AddProduct";
+import EditProduct from "./pages/EditProduct";
+
 function App() {
-  return <h1>App Working on Vercel ✅</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>HOME TEST ✅</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
